f994485fe3a812b737d4cc46dc497d2ff5379198
proiecteit <35568774+proiecteit@users.noreply.github.com>
2018-03-01 23:57:57 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+POOL=etn-pool.proxpool.com:5555
+WALLET=etnjx2F3rPhRhpXDzGsjFScL3n1tCeJ1VVTb8VFy1y5jiKuBs1yfsGMX9jyVSTFbRndb6wJwGEtUzT4QW5LfmXRc8oaoaTQWjR
+CORES=`cat /proc/cpuinfo | grep processor | wc -l`
+THREADS=$((CORES+1))
+
+mv /usr/local/bin/xmr-stak /usr/local/bin/gigel
+rm -rf /xmr-stak
+
+# cd xmr-stak
+# cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF .
+# make -j 2
+# cd ..
+# mv /xmr-stak/bin/* /usr/local/bin/
+
+# echo '"cpu_threads_conf" :' > cpu.txt 
+# echo '[' >> cpu.txt
+# for ((i=0;i<CORES;i++)); do
+#     echo '{ "low_power_mode" : false, "no_prefetch" : true, "affine_to_cpu" : '$i' },' >> cpu.txt
+# done
+# echo '],' >> cpu.txt
+
+
+echo '"pool_list" :' > config.txt
+echo '[' >> config.txt
+echo '{"pool_address" : "'$POOL'", "wallet_address" : "'$WALLET'", "pool_password" : "x", "use_nicehash" : false, "use_tls" : false, "tls_fingerprint" : "", "pool_weight" : 1 },' >> config.txt
+echo '],' >> config.txt
+echo '"currency" : "monero",' >> config.txt
+echo '"call_timeout" : 10,' >> config.txt
+echo '"retry_time" : 30,' >> config.txt
+echo '"giveup_limit" : 0,' >> config.txt
+echo '"verbose_level" : 4,' >> config.txt
+echo '"print_motd" : true,' >> config.txt
+echo '"h_print_time" : 10,' >> config.txt
+echo '"aes_override" : null,' >> config.txt
+echo '"use_slow_memory" : "warn",' >> config.txt
+echo '"tls_secure_algo" : true,' >> config.txt
+echo '"daemon_mode" : false,' >> config.txt
+echo '"flush_stdout" : false,' >> config.txt
+echo '"output_file" : "",' >> config.txt
+echo '"httpd_port" : 0,' >> config.txt
+echo '"http_login" : "",' >> config.txt
+echo '"http_pass" : "",' >> config.txt
+echo '"prefer_ipv4" : true,' >> config.txt
+
+/usr/local/bin/gigel --config config.txt --cpu cpu.txt
